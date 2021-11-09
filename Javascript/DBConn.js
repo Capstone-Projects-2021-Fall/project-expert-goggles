c43848a6d2906a92d706e1f61f992ab8de6813d2
Eddie-Brace <tun38842@temple.edu>
2021-11-09 17:31:24 -0500

    saves user D3 history automatically without manual login

--- Javascript/DBConn.js
+++ Javascript/DBConn.js
@@ -23,6 +23,7 @@ const firebaseConfig = {
 // Initialize Firebase, database object, and Local D3InfoObj storage
 firebase.initializeApp(firebaseConfig);
 const vis_db = firebase.firestore().collection("visualizations");
+const his_db = firebase.firestore().collection("UserHistories");
 
 firebase.auth().signInAnonymously()
   .then(() => {
@@ -33,18 +34,16 @@ firebase.auth().signInAnonymously()
     var errorMessage = error.message;
     console.log("sign-in failed")
   });
-
+var uid;
 firebase.auth().onAuthStateChanged((user) => {
 if (user) {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/firebase.User
-    var uid = user.uid;
-    // ...
-} else {
-    // User is signed out
-    // ...
-}
+    uid = user.uid;
+    console.log("obtained user id");
+} else { console.log(" User is signed out"); }
 });
+
 
 //The fetchGuide(type) function runs a query to the firestore database
 //to check if the type argument passed to it has a guide associated with it
@@ -66,6 +65,7 @@ async function fetchGuide()
         myD3.guide = query.data();
         console.log("Fetched a guide for " + type + " and forwarding to UI.");
         sendToUI(myD3);
+        saveToHistory(myD3);
     }
 }
 
@@ -95,7 +95,7 @@ chrome.runtime.onMessage.addListener(
     myD3 = D3InfoObj;
     myD3.tab = sender.tab.id;
     console.log("Received a guide request from the Parser for " + myD3.type);
-
+    myD3.url = sender.tab.url;
 
     //If the Parser determined there was no D3 on a page or if there was an error,
     //do nothing and make sure the extension isn't showing anything
@@ -107,3 +107,40 @@ chrome.runtime.onMessage.addListener(
         fetchGuide();
   }
 );
+
+//saves document to database containing ID of relevant user,
+//vis. type, URL, and date last accessed
+function saveToHistory(sentObj) {
+
+  his_db.doc(jenkinsOneAtATimeHash(uid + String(sentObj.url))).set({
+      user: uid,
+      type: sentObj.type,
+      url: String(sentObj.url),
+      last_accessed: firebase.firestore.Timestamp.now()
+  })
+  .then(() => {
+    console.log("document saved to history");
+  })
+  .catch((error) => {
+    console.error("Error saving doc: ", error);
+  });
+
+//Credits (modified code): Bob Jenkins (http://www.burtleburtle.net/bob/hash/doobs.html)
+//See also: https://en.wikipedia.org/wiki/Jenkins_hash_function
+//Takes a string of any size and returns an avalanching hash string of 8 hex characters.
+function jenkinsOneAtATimeHash(keyString)
+{
+  let hash = 0;
+  for (let charIndex = 0; charIndex < keyString.length; ++charIndex)
+  {
+    hash += keyString.charCodeAt(charIndex);
+    hash += hash << 10;
+    hash ^= hash >> 6;
+  }
+  hash += hash << 3;
+  hash ^= hash >> 11;
+  //4,294,967,295 is FFFFFFFF, the maximum 32 bit unsigned integer value, used here as a mask.
+  return (((hash + (hash << 15)) & 4294967295) >>> 0).toString(16)
+};
+
+}
