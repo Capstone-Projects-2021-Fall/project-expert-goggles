d1caf4ba0d596e914791ea3104f887ed759f7041
Josh Withka <tuj84619@temple.edu>
2021-11-09 19:22:23 -0500

    Added new tab functionality for iframes
    If the list of D3 library functions called on a webpage is empty, null, or undefined, AND an iframe is present on the page, the user is prompted to open it in a new tab.

--- Javascript/Parser.js
+++ Javascript/Parser.js
@@ -10,6 +10,8 @@ function sendToDB(sentObj)
     catch(err) {console.log("Error in sendToDB()");}
 }
 
+
+
 function parseType(parseInfo)
 {
     //funcList is a list of tracked function calls from
@@ -17,12 +19,31 @@ function parseType(parseInfo)
     console.log(parseInfo.funcList);
     var funcList = [...parseInfo.funcList];
 
+    // If funcList is empty...
+    if(funcList.length == 0 || funcList == null){
+        // check for an iframe. 
+        var iframes = document.getElementsByTagName('iframe');
+        if(!(iframes.length == 0)){
+            if(confirm("Could not locate a D3, should we open the iframe in a new tab?")){
+                window.open(iframes[0].src);
+            }
+            else{
+                console.log("User did not want to open the iframe.");
+            }
+        }
+        console.log("No D3 or iframe located.");
+    }
+    // if it exists, prompt the user to open it in a new tab
+    // then possibly run the method to check the savedHTML JSON's URL value and compare it to the iframe URL
+    // if it is a match, then the new tab that was opened gave us the contents of the iframe
+
+
     //Default: Unsupported
     D3InfoObj.type = "unsupported";
 
 
     //Logic:
-    waitForJson();
+    //waitForJson();
 
     var possType;
     var funcListLen = funcList.length;
@@ -55,6 +76,20 @@ function parseType(parseInfo)
     console.log("It is likely: " + possType);
     D3InfoObj.type = possType;
     sendToDB(D3InfoObj);
+
+    if(funcList.length == 0 || funcList == null || possType == undefined){
+        // check for an iframe. 
+        var iframes = document.getElementsByTagName('iframe');
+        if(!(iframes.length == 0)){
+            if(confirm("Could not locate a D3, should we open the iframe in a new tab?")){
+                window.open(iframes[0].src);
+            }
+            else{
+                console.log("User did not want to open the iframe.");
+            }
+        }
+        console.log("No D3 or iframe located.");
+    }
 
 }
 
