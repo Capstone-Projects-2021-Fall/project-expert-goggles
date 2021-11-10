4e9337e55ba1ab84c3ebf8c5765e68d53471119f
Josh Withka <tuj84619@temple.edu>
2021-11-09 19:44:33 -0500

    Removed redundant Parser code for iframe handling

--- Javascript/Parser.js
+++ Javascript/Parser.js
@@ -10,33 +10,12 @@ function sendToDB(sentObj)
     catch(err) {console.log("Error in sendToDB()");}
 }
 
-
-
 function parseType(parseInfo)
 {
     //funcList is a list of tracked function calls from
     //the modified D3 source code
     console.log(parseInfo.funcList);
     var funcList = [...parseInfo.funcList];
-
-    // If funcList is empty...
-    if(funcList.length == 0 || funcList == null){
-        // check for an iframe. 
-        var iframes = document.getElementsByTagName('iframe');
-        if(!(iframes.length == 0)){
-            if(confirm("Could not locate a D3, should we open the iframe in a new tab?")){
-                window.open(iframes[0].src);
-            }
-            else{
-                console.log("User did not want to open the iframe.");
-            }
-        }
-        console.log("No D3 or iframe located.");
-    }
-    // if it exists, prompt the user to open it in a new tab
-    // then possibly run the method to check the savedHTML JSON's URL value and compare it to the iframe URL
-    // if it is a match, then the new tab that was opened gave us the contents of the iframe
-
 
     //Default: Unsupported
     D3InfoObj.type = "unsupported";
@@ -77,6 +56,7 @@ function parseType(parseInfo)
     D3InfoObj.type = possType;
     sendToDB(D3InfoObj);
 
+    // If no substantial results are found from D3 scraping...
     if(funcList.length == 0 || funcList == null || possType == undefined){
         // check for an iframe. 
         var iframes = document.getElementsByTagName('iframe');
