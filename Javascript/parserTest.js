48098e976d9dc8bf31ca693f95064fdd6e69c087
Conlin Fox <tuh26961@temple.edu>
2021-12-05 12:49:26 -0500

    Added tests for majority of available guides

--- Javascript/parserTest.js
+++ Javascript/parserTest.js
@@ -51,6 +51,41 @@ visHistogram.argList = [];
 visHistogram.sender = "ExpertGoggles";
 visHistogram.iframeList = [];
 
+const visPieChart = {};
+visPieChart.funcList = ["select", "scaleOrdinal", "pie", "arc", "csv"];
+visPieChart.argList = [];
+visPieChart.sender = "ExpertGoggles";
+visPieChart.iframeList = [];
+
+const visScatterPlot = {};
+visScatterPlot.funcList = ["linear", "0", "category10", "ordinal", "select", "qualify", "csv", "dispatch", "rebind", "keys", "merge", "extent", "axis", "range", "format", "transition"];
+visScatterPlot.argList = ["circle", "cx", "cy"];
+visScatterPlot.sender = "ExpertGoggles";
+visScatterPlot.iframeList = [];
+
+const visSequencesSunburst = {};
+visSequencesSunburst.funcList = ["category20c", "ordinal", "select", "qualify", "partition", "hierarchy", "rebind", "arc", "json", "dispatch", "selectAll"];
+visSequencesSunburst.argList = [];
+visSequencesSunburst.sender = "ExpertGoggles";
+visSequencesSunburst.iframeList = [];
+
+const visStackedAreaChart = {};
+visStackedAreaChart.funcList = ["select", "timeParse", "scaleTime", "timeFormat", "scaleLinear", "scaleOrdinal", "stack", "area", "tsv", "extent", "axisBottom", "axisLeft", "format"];
+visStackedAreaChart.argList = [];
+visStackedAreaChart.sender = "ExpertGoggles";
+visStackedAreaChart.iframeList = [];
+
+const visStackedBarChart = {};
+visStackedAreaChart.funcList = ["select", "scaleBand", "scaleLinear", "scaleOrdinal", "csv", "max", "stack", "axisBottom", "axisLeft", "formatPartition"];
+visStackedBarChart.argList = [];
+visStackedBarChart.sender = "ExpertGoggles";
+visStackedBarChart.iframeList = [];
+
+const visTreemap = {};
+visTreemap.funcList = ["select", "interpolateRGB", "scaleOrdinal", "format", "treemap", "json", "hierarchy", "treemapResquarify", "selectAll", "timeout"];
+visTreemap.argList = [];
+visTreemap.sender = "ExpertGoggles";
+visTreemap.iframeList = [];
 
 describe('All guide types should be properly recognized', () => {
     it('Line Charts should be recognized', () => {
@@ -68,9 +103,8 @@ describe('All guide types should be properly recognized', () => {
      it('Circle Packing Charts should be recognized', () => {
         expect(parseType(visCirclePackingChart)).to.equal('circle_packing_chart') 
      })
-     /*
      it('Difference Chart should be recognized', () => {
-        expect(parseType(visDifferenceChart)).to.equal('Difference chart')
+        expect(parseType(visDifferenceChart)).to.equal('Difference Chart')
      })
      it('Heatmap should be recognized', () => {
         expect(parseType(visHeatmap)).to.equal('heatmap')
@@ -78,5 +112,22 @@ describe('All guide types should be properly recognized', () => {
      it('Histogram should be recognized', () => {
         expect(parseType(visHistogram)).to.equal('histogram')
      })
-     */
+     it('Pie Chart should be recognized', () => {
+        expect(parseType(visPieChart)).to.equal('pie_chart')
+     })
+     it('Scatter Plot should be recognized', () => {
+        expect(parseType(visScatterPlot)).to.equal('scatter_plot')
+     })
+     it('Sequences Sunburst should be recognized', () => {
+        expect(parseType(visSequencesSunburst)).to.equal('sequences_sunburst')
+     })
+     it('Stacked Area Chart should be recognized', () => {
+        expect(parseType(visStackedAreaChart)).to.equal('stacked_area_chart')
+     })
+     it('Stacked Bar Chart should be recognized', () => {
+        expect(parseType(visStackedBarChart)).to.equal('stacked_bar_chart')
+     })
+     it('Treemap should be recognized', () => {
+        expect(parseType(visTreemap)).to.equal('treemap')
+     })
 })
